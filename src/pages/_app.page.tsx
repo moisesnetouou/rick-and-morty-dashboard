0d55278626ae92dbce5ce83b50b7a702d3d5b014
Moisés Neto <moisesnetored@gmail.com>
2023-03-28 22:13:55 -0400

    feat: add react query, react query tools, axios and graphql

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,13 +1,20 @@
+import { queryClient } from '@/services/queryClient';
 import '@/styles/globals.css';
+
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import type { AppProps } from 'next/app';
 
 import { GlobalStyle } from '../styles/global';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
