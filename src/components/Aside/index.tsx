7cb5789ad038bcea48a90997984ec1b8226ce383
Moisés Neto <moisesnetored@gmail.com>
2023-03-29 22:44:45 -0400

    feat: adjust aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,10 +1,18 @@
+import Image from 'next/image';
 import { AsideContainer } from './styles';
+import logoImg from '../../../public/assets/images/rick-and-morty-logo.png';
 
 export function Aside() {
   return (
     <AsideContainer>
       <div>
-        <h2>first</h2>
+        <Image src={logoImg} alt="logo rick and morty" width={280} />
+
+        <div>
+          <h2>Dashboard</h2>
+          <h2>Personagens</h2>
+          <h2>Episódios</h2>
+        </div>
       </div>
     </AsideContainer>
   );
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -1,13 +1,24 @@
 import styled from 'styled-components';
 
 export const AsideContainer = styled.aside`
-  background-color: red;
+  background-color: ${(props) => props.theme['yellow-400']};
   width: 20rem;
   height: 100vh;
 
   > div {
     width: 20rem;
     height: 100%;
-    background-color: red;
+    padding: 2rem;
+    justify-content: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    > div {
+      width: 100%;
+      margin-top: 4rem;
+
+      color: ${(props) => props.theme['blue-800']};
+    }
   }
 `;
